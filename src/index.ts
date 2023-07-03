119600de99ad5d6ebeabfdf617494304b1dc88b2
彭鹏 <pengpeng@pengpengdeMacBook-Pro.local>
2023-07-03 21:13:25 +0800

    feat: 完成功能

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
+import chroma from "chroma-js";
+
 const range = document.getElementById("range") as HTMLInputElement;
 const box = document.querySelector(".box") as HTMLDivElement;
+
+const boxPosition = box.getBoundingClientRect();
+const rangePosition = range.getBoundingClientRect();
+const boxStartX = boxPosition.width / 2;
+const boxEndX = rangePosition.width - boxPosition.width / 2;
+
+function getTransitionColor(
+  color1: string,
+  color2: string,
+  percentage: number
+) {
+  const rgb1 = chroma(color1).css();
+  const rgb2 = chroma(color2).css();
+  return chroma.mix(rgb1, rgb2, percentage / 100);
+}
+
+const handleRangeValueChange = (e: Event) => {
+  if (!e.target) return;
+  const percentValue = Number((e.target as HTMLInputElement).value);
+  const stepWidth = Math.round((boxEndX - boxStartX) / 100) * percentValue;
+  box.style.left = `${stepWidth}px`;
+  box.style.background = getTransitionColor(
+    "green",
+    "yellow",
+    percentValue
+  ).css();
+  box.style.borderRadius = `${percentValue}%`;
+};
+
+range.addEventListener("input", handleRangeValueChange);
